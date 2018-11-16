2f8b714767412592fa0605bf474108f3638910fa
Andrew M. Silva <andrewsaxx@gmail.com>
2018-11-16 15:34:08 -0200

    New template, section Sobre and section Notícias

--- main/views.py
+++ main/views.py
@@ -3,5 +3,22 @@ from django.utils import timezone
 from .models import Noticia
 
 def index(request):
-	noticias = Noticia.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
-	return render(request, 'main/index.html', {'noticias': noticias})
+	array_noticias = []
+	noticias = Noticia.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')[:6]
+	cont = 0
+	i = 0
+	for noticia in noticias:
+		if(cont == 0):
+			array_noticias.append([])
+
+		if(len(noticia.texto) > 165):
+			noticia.texto = noticia.texto[:165]+"..."
+
+		array_noticias[i].append(noticia)
+		cont += 1
+
+		if(cont == 3):
+			cont = 0
+			i += 1
+
+	return render(request, 'main/index.html', {'array_noticias': array_noticias})
