153a3be80210ea216785f27fdcd8666de891878a
Andrew M. Silva <andrewsaxx@gmail.com>
2018-11-16 00:05:22 -0200

    Starting index

--- main/views.py
+++ main/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Noticia
 
 def index(request):
-	return render(request, 'main/index.html', {})
+	noticias = Noticia.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
+	return render(request, 'main/index.html', {'noticias': noticias})
