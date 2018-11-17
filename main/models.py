e3b59d3bd6c3141f11d91b6e1723011c7d3931bc
Andrew M. Silva <andrewsaxx@gmail.com>
2018-11-16 23:34:55 -0200

    Cronograma with bugs

--- main/models.py
+++ main/models.py
@@ -5,18 +5,14 @@ class Noticia(models.Model):
 	autor = models.ForeignKey('auth.user', on_delete=models.CASCADE)
 	titulo = models.CharField(max_length=60)
 	texto = models.TextField()
-	imagem = models.ImageField(null=True)
+	imagem = models.ImageField(upload_to='media', null=True, blank=True)
 	data_publicacao = models.DateTimeField(default=timezone.now)
-
-	def __str__(self):
-		return self.titulo
 
 class Atracao(models.Model):
 	autor = models.ForeignKey('auth.user', on_delete=models.CASCADE)
 	titulo = models.CharField(max_length=60)
 	descricao = models.TextField()
-	imagem = models.ImageField(null=True)
+	imagem = models.ImageField(upload_to='media', null=True, blank=True)
 	data_inicio = models.DateTimeField(default=timezone.now)
 	data_fim = models.DateTimeField(default=timezone.now)
-
 
--- main/views.py
+++ main/views.py
@@ -1,18 +1,17 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Noticia
+from .models import Atracao
 
 def index(request):
+	# Obtendo notícias
 	array_noticias = []
-	noticias = Noticia.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')[:6]
+	noticias = Noticia.objects.filter(data_publicacao__lte=timezone.now()).order_by('-data_publicacao')[:6]
 	cont = 0
 	i = 0
 	for noticia in noticias:
 		if(cont == 0):
 			array_noticias.append([])
-
-		if(len(noticia.texto) > 165):
-			noticia.texto = noticia.texto[:165]+"..."
 
 		array_noticias[i].append(noticia)
 		cont += 1
@@ -21,4 +20,25 @@ def index(request):
 			cont = 0
 			i += 1
 
-	return render(request, 'main/index.html', {'array_noticias': array_noticias})
+	# Obtendo dias de evento
+	atracoes = Atracao.objects.filter(data_inicio__gte=timezone.now()).order_by('data_inicio')
+	datas = []
+	for atracao in atracoes:
+		inserir = True
+		for data in datas:
+			if(atracao.data_inicio.day == data.day and atracao.data_inicio.month == data.month and atracao.data_inicio.year == data.year):
+				inserir = False
+				break
+		if(inserir):
+			datas.append(atracao.data_inicio)
+
+	# Obtendo atrações
+	array_atracoes = [[]]*len(datas)
+	for atracao in atracoes:
+		for i in range(0, len(datas)):
+			if(atracao.data_inicio.day == datas[i].day and atracao.data_inicio.month == datas[i].month and atracao.data_inicio.year == datas[i].year):
+				array_atracoes[i].append(atracao)
+	print(array_atracoes)
+
+
+	return render(request, 'main/index.html', {'array_noticias': array_noticias,'datas': datas, 'atracoes': atracoes})
