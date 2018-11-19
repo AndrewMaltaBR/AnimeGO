245bc8c61acb71e07a0a0e74979ce769a7182dfa
Andrew M. Silva <andrewsaxx@gmail.com>
2018-11-18 23:57:24 -0200

    ER, models and Cronograma done

--- main/models.py
+++ main/models.py
@@ -17,3 +17,16 @@ class Atracao(models.Model):
 	local = models.CharField(max_length=120, null=True)
 	data_inicio = models.DateTimeField(default=timezone.now)
 	data_fim = models.DateTimeField(default=timezone.now)
+
+class Estande(models.Model):
+	usuario = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+	nome = models.CharField(max_length=60)
+	descricao = models.TextField()
+	imagem = models.ImageField(upload_to='media', null=True, blank=True)
+	nome_responsavel = models.CharField(max_length=120, null=True)
+	local = models.CharField(max_length=120, null=True)
+
+class Foto(models.Model):
+	usuario = models.ForeignKey('auth.user', on_delete=models.CASCADE)
+	imagem = models.ImageField(upload_to='media', null=True, blank=True)
+	descricao = models.TextField()
